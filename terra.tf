9ed33927aa56ef315393ea297cae2c39225689d5
darek73pl <dkurzynski@gazeta.pl>
2019-12-05 22:07:42 +0100

    as

--- terra.tf
+++ terra.tf
@@ -1,4 +1,4 @@
-terraform {
+/*terraform {
   backend "azurerm" {
     resource_group_name  = "Constant-RG"
     storage_account_name = "dartest01"
@@ -6,7 +6,7 @@ terraform {
     key                  = "terraform.tfstate"
   }
 }
-
+*/
 provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = "=1.36.0"
